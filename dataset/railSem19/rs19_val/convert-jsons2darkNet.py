2b52898e4137ce74021cc2ca5141b06af134c704
SebiGoebel <sebastian.goebel10@gmail.com>
2024-04-05 10:16:38 +0200

    included a description of the convert script

--- dataset/railSem19/rs19_val/convert-jsons2darkNet.py
+++ dataset/railSem19/rs19_val/convert-jsons2darkNet.py
@@ -1,5 +1,81 @@
 import json
 
+"""
+This script converts the label-format of RailSem19 into the label-format of DarkNet which is usually used by yolos.
+Examples of this formats are provided in the following.
+
+RailSem19 format:
+
+{
+    "frame": "rs00000", 
+    "imgHeight": 1080, 
+    "imgWidth": 1920, 
+    "objects": [
+        {
+            "boundingbox": [
+                1066, 
+                571, 
+                1077, 
+                590
+            ], 
+            "label": "track-sign-front"
+        }, 
+        {
+            "boundingbox": [
+                1024, 
+                599, 
+                1052, 
+                612
+            ], 
+            "label": "switch-unknown"
+        },  
+        {
+            "label": "pole", 
+            "polygon": [
+                [
+                    1084, 
+                    527
+                ],
+                ...
+                [
+                    1084, 
+                    520
+                ]
+            ]
+        },  
+        {
+            "label": "pole", 
+            "polygon": [
+                [
+                    1125, 
+                    535
+                ], 
+                ... 
+                [
+                    1128, 
+                    534
+                ]
+            ]
+        },
+    ]
+}
+
+DarkNet format:
+
+the format of each row:
+class_id center_x center_y width height
+
+img0001.txt:
+1 0.408 0.30266666666666664 0.104 0.15733333333333333
+1 0.245 0.424 0.046 0.08
+
+darknet.labels:
+head
+helmet
+person
+
+"""
+
 def read_json_file(file_path):
     """Read JSON file and return its content."""
     with open(file_path, 'r') as file:
@@ -27,7 +103,19 @@ if __name__ == "__main__":
     print("frame:", frame)
     print("imgHeight:", imgHeight)
     print("imgWidth:", imgWidth)
-    print("Bounding Boxes with Labels:")
+
+    print("--------------- Bounding Boxes with Labels: ---------------")
+
     for bounding_box in bounding_boxes_with_labels:
         print("Label:", bounding_box['label'])
         print("Bounding Box:", bounding_box['boundingbox'])
+
+    print("--------------- Bounding Boxes with Labels with counter: ---------------")
+
+    for i, bounding_box in enumerate(bounding_boxes_with_labels, start=1):
+        print(f"Bounding Box {i}:")
+        print("Label:", bounding_box['label'])
+        print("Bounding Box:", bounding_box['boundingbox'])
+
+    # Print the total number of bounding boxes
+    print(f"Total Bounding Boxes: {len(bounding_boxes_with_labels)}")
