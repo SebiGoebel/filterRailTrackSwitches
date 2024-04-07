f3eeaad27182dadb71a7b96240cc1b58b8aeb237
SebiGoebel <sebastian.goebel10@gmail.com>
2024-04-07 14:11:53 +0200

    counting labels for test folder (first 5 jsons) is complete

--- dataset/railSem19/rs19_val/counting_labels.py
+++ dataset/railSem19/rs19_val/counting_labels.py
@@ -83,137 +83,50 @@ def read_json_file(file_path):
         data = json.load(file)
     return data
 
-# ---------------- writing lables to txt ----------------
-# writing labels to txt (darknet.labls)
-def write_labels_to_file_darknet(list, file_path):
-    with open(file_path, 'w') as file:
-        for index, item in enumerate(list):
-            file.write(str(item))
-            if index != len(list) - 1: # after last label no '\n'
-                file.write('\n')
-
-# writing lables to txt
-def write_multiple_labels_to_file(labels, file_path):
-    with open(file_path, 'w') as file:
-        for i, line_labels in enumerate(labels):
-            line = ' '.join(map(str, line_labels))
-            if i < len(labels) - 1:  # after last label no '\n'
-                file.write(line + '\n')
-            else:
-                file.write(line)
-
-# check and update label list
-def check_and_update_label_list(label, label_list):
-    if label in label_list:
-        return label_list.index(label)
-    else:
-        label_list.append(label)
-        return len(label_list) - 1
-
-# converting bounding box parameters form railSem19-format to darkNet-format
-def converting_bounding_boxes_parameters(current_label, current_bounding_box, imgWidth, imgHeight):
-    # first assumption: points of bounding_box is top-left and bottom-right
-    top_left_x = current_bounding_box[0]
-    top_left_y = current_bounding_box[1]
-    bottom_right_x = current_bounding_box[2]
-    bottom_right_y = current_bounding_box[3]
-    
-    # calculating new parameters of bounding boxes
-    x_centre = (top_left_x + (bottom_right_x - top_left_x) / 2) / imgWidth
-    y_centre = (top_left_y + (bottom_right_y - top_left_y) / 2) / imgHeight
-    w_rel = (bottom_right_x - top_left_x) / imgWidth
-    h_rel = (bottom_right_y - top_left_y) / imgHeight
-    
-    # writing numbers to .txt file
-    label_number = check_and_update_label_list(current_label, label_list)
-    new_bounding_box = [label_number, x_centre, y_centre, w_rel, h_rel]
-    return new_bounding_box
-
-# converting label from text to numbers
-def counting_labels(label):
-    if label == "abc":
-        return 1
-    elif label == "klj":
-        return 2
-    elif label == "xyz":
-        return 3
-    elif label == "udf":
-        return 4
-    else:
-        return 0  # 0 --> no label fits
-
-def converting_single_json(json_file_path, write_folder_path, darknet_filename):
-    # reading from single json file
-    #json_file_path = read_folder_path + "rs" + str(json_number).zfill(5) + ".json"
-    json_content = read_json_file(json_file_path)
-
-    # Extract required fields
-    frame = json_content['frame']
-    imgHeight = json_content['imgHeight']
-    imgWidth = json_content['imgWidth']
-
-    # Extract boundingbox and label from objects
-    bounding_boxes_with_labels = []
-    for obj in json_content['objects']:
-        if 'boundingbox' in obj and 'label' in obj:
-            label = obj['label']
-            bounding_box = obj['boundingbox']
-            bounding_boxes_with_labels.append({'label': label, 'boundingbox': bounding_box})
-            
-    print("converting label data of frame: ", frame)
-
-    all_labels_current_image = []
-
-    # convertion of label-data
-    for bounding_box in bounding_boxes_with_labels:
-        current_label = bounding_box['label']
-        print("current Label:", current_label)
-
-        current_bounding_box = bounding_box['boundingbox']
-        new_bounding_box = converting_bounding_boxes_parameters(current_label, current_bounding_box, imgWidth, imgHeight)
-
-        # add to all_labels list
-        all_labels_current_image.append(new_bounding_box)
-
-    print(all_labels_current_image)
-
-    # writing converted labels to .txt file
-    write_file_path = write_folder_path + frame + ".txt"
-    write_multiple_labels_to_file(all_labels_current_image, write_file_path)
-
-    print(label_list)
-
-    # writing labels to darknet.label file
-    write_darknet_path = write_folder_path + darknet_filename
-    write_labels_to_file_darknet(label_list, write_darknet_path)
-
-
-
-
 if __name__ == "__main__":
-    #file_path = input("Enter the path to the JSON file: ") # input in terminal
-    #read_file_path = "jsons/rs19_val/rs00000.json"          # hardcoded for rs00000.json
-
     # paths for read and write folders and files
     #read_folder_path = "jsons/rs19_val/"
     read_folder_path = "jsons/test/"
-    write_folder_path = "darknets/"
-    darknet_filename = "darknet.labels"
 
-    #json_content = read_json_file(read_file_path)
-
-    label_list = []
+    crossing_counter = 0
+    track_sign_front_counter = 0
+    switch_unknown_counter = 0
+    error_counter = 0
 
     for filename in os.listdir(read_folder_path):
         file_path = os.path.join(read_folder_path, filename)
         if os.path.isfile(file_path):
             print(file_path)
-            converting_single_json(file_path, write_folder_path, darknet_filename)
-            
-            # create new filenames
-            #name, extension = os.path.splitext(filename) # split filename and extention
-            #new_filename = name + txt_extension # set txt extention
-            #print(new_filename)
+            json_content = read_json_file(file_path)
+        
+            # Extract required fields
+            frame = json_content['frame']
+            print("frame: ", frame)
 
+            # Extract boundingbox and label from objects
+            bounding_boxes_with_labels = []
+            for obj in json_content['objects']:
+                if 'boundingbox' in obj and 'label' in obj:
+                    label = obj['label']
+                    bounding_box = obj['boundingbox']
+                    bounding_boxes_with_labels.append({'label': label, 'boundingbox': bounding_box})
+
+            # convertion of label-data
+            for bounding_box in bounding_boxes_with_labels:
+                current_label = bounding_box['label']
+                print("current Label:", current_label)
+
+                if current_label == "crossing":
+                    crossing_counter += 1
+                elif current_label == "track-sign-front":
+                    track_sign_front_counter += 1
+                elif current_label == "switch-unknown":
+                    switch_unknown_counter += 1
+                else:
+                    error_counter += 1  # no label fits
     
-    # ACHUTUNG FEHLER!!! --> jeder durchlauf (jedes json) hat eine eigene bzw. neue darknet.label liste und überschreibt das darknet.labels file
+    print("crossing_counter: ", crossing_counter)
+    print("track_sign_front_counter: ", track_sign_front_counter)
+    print("switch_unknown_counter: ", switch_unknown_counter)
+    print("error_counter: ", error_counter)
+
