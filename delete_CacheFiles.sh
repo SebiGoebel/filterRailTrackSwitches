1321dfa39e74c0e13eb4fb9f7bb91b8875bc17ce
SebiGoebel <sebastian.goebel10@gmail.com>
2024-04-24 13:57:29 +0200

    changed paths in delete_CachFiles to the right ones

--- delete_CacheFiles.sh
+++ delete_CacheFiles.sh
@@ -2,12 +2,12 @@
 
 # Array mit Dateipfade
 dateien=(
-    "/srv/cdl-eml/datasets/railsem19/rs19_split8500/train/labels.cache"                 # rs19_split8500 train
-    "/srv/cdl-eml/datasets/railsem19/rs19_split8500/test/labels.cache"                  # rs19_split8500 test
-    "/srv/cdl-eml/datasets/railsem19/rs19_split8500/valid/labels.cache"                 # rs19_split8500 valid
-    "/srv/cdl-eml/datasets/railsem19/rs19_split8500_onlySwitches/train/labels.cache"    # rs19_split8500_onlySwitches train
-    "/srv/cdl-eml/datasets/railsem19/rs19_split8500_onlySwitches/test/labels.cache"     # rs19_split8500_onlySwitches test
-    "/srv/cdl-eml/datasets/railsem19/rs19_split8500_onlySwitches/valid/labels.cache"    # rs19_split8500_onlySwitches valid
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/train/labels.cache"                 # rs19_split8500 train
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/test/labels.cache"                  # rs19_split8500 test
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/valid/labels.cache"                 # rs19_split8500 valid
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/train/labels.cache"    # rs19_split8500_onlySwitches train
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/test/labels.cache"     # rs19_split8500_onlySwitches test
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/valid/labels.cache"    # rs19_split8500_onlySwitches valid
 )
 
 # überprüfe und lösche jede Datei
