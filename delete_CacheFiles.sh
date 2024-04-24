e1b16090677f4bcfeec53d6d2d49a3c89f7533f0
SebiGoebel <sebastian.goebel10@gmail.com>
2024-04-24 14:07:56 +0200

    translated some comments to english in delete_CacheFiles.sh

--- delete_CacheFiles.sh
+++ delete_CacheFiles.sh
@@ -3,7 +3,7 @@
 # this skript deletes all labels.cache files in the dataset-split-directory
 # this is needed before training another model (e.g.: training yolov7 after the training of yolov9 created lables.cache files)
 
-# Array mit Dateipfade
+# array with all file paths
 dateien=(
     "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/train/labels.cache"                 # rs19_split8500 train
     "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/test/labels.cache"                  # rs19_split8500 test
@@ -13,7 +13,7 @@ dateien=(
     "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/valid/labels.cache"    # rs19_split8500_onlySwitches valid
 )
 
-# überprüfe und lösche jede Datei
+# check and delete each file
 for datei in "${dateien[@]}"; do
     if [ -f "$datei" ]; then
         rm "$datei"
