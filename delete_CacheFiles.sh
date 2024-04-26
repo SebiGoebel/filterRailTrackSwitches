9fafd3db2f0ece985a8a706f67c3b0855ca50646
SebiGoebel <sebastian.goebel10@gmail.com>
2024-04-26 09:53:28 +0200

    updated delete_cachefile skript with new dataset paths

--- delete_CacheFiles.sh
+++ delete_CacheFiles.sh
@@ -5,12 +5,15 @@
 
 # array with all file paths
 dateien=(
-    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/train/labels.cache"                 # rs19_split8500 train
-    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/test/labels.cache"                  # rs19_split8500 test
-    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/valid/labels.cache"                 # rs19_split8500 valid
-    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/train/labels.cache"    # rs19_split8500_onlySwitches train
-    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/test/labels.cache"     # rs19_split8500_onlySwitches test
-    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/valid/labels.cache"    # rs19_split8500_onlySwitches valid
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/train/labels.cache"                        # rs19_split8500 train
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/test/labels.cache"                         # rs19_split8500 test
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500/valid/labels.cache"                        # rs19_split8500 valid
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/train/labels.cache"           # rs19_split8500_onlySwitches train
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/test/labels.cache"            # rs19_split8500_onlySwitches test
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitches/valid/labels.cache"           # rs19_split8500_onlySwitches valid
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitchesLeftRight/train/labels.cache"  # rs19_split8500_onlySwitchesLeftRight train
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitchesLeftRight/test/labels.cache"   # rs19_split8500_onlySwitchesLeftRight test
+    "/home/sebi/filterRailTrackSwitches/dataset/railSem19/rs19_split8500_onlySwitchesLeftRight/valid/labels.cache"  # rs19_split8500_onlySwitchesLeftRight valid
 )
 
 # check and delete each file
