39acb8251156749df0d918d1ecb01783c6bee9ac
SebiGoebel <sebastian.goebel10@gmail.com>
2024-07-13 12:09:15 +0200

    created a controlling file for checking if the polylines are correctly labeled and added finished temporalSwitchDataset labels in a .zip folder

---
+++ dataset/temporaldataset/controllingTemporalDataset.py
@@ -0,0 +1,104 @@
+"""
+FORMAT:
+
+{
+    "info": {},
+    "categories": {
+        "label": {
+            "labels": [
+                {"name": "left_rail", "parent": "", "attributes": []},
+                {"name": "right_rail", "parent": "", "attributes": []}
+            ],
+            "attributes": ["occluded"]
+        },
+        "points": {"items": []}
+    },
+    "items": [
+        {
+            "id": "images/temporalRails/sequnce_1074_frame_026820",
+            "annotations": [
+                {
+                    "id": 0,
+                    "type": "polyline",
+                    "attributes": {"occluded": false},
+                    "group": 0,
+                    "label_id": 1,
+                    "points": [778.8, 720.0, 777.0, 714.0, 775.0, 709.0, 773.0, 704.0, 771.0, 699.0, 769.0, 694.0, 767.0, 689.0, 765.0, 684.0, 764.0, 679.0, 762.0, 674.0, 760.0, 669.0, 758.0, 664.0, 757.0, 659.0, 755.0, 654.0, 753.0, 649.0, 751.0, 644.0, 749.0, 639.0, 748.0, 634.0, 747.0, 629.0, 745.0, 624.0, 744.0, 619.0, 742.0, 614.0, 741.0, 609.0, 739.0, 604.0, 738.0, 599.0, 737.0, 594.0, 737.0, 589.0, 736.0, 584.0, 735.0, 579.0, 735.0, 574.0, 735.0, 569.0, 735.0, 564.0, 735.0, 559.0, 735.0, 554.0, 736.0, 549.0, 737.0, 544.0, 738.0, 539.0, 741.0, 534.0, 744.0, 529.0, 747.0, 524.0, 751.0, 519.0, 757.0, 514.0, 763.0, 509.0, 772.0, 504.0, 782.0, 499.0, 795.0, 494.0],
+                    "z_order": 0
+                },
+                {
+                    "id": 0,
+                    "type": "polyline",
+                    "attributes": {"occluded": false},
+                    "group": 0,
+                    "label_id": 0,
+                    "points": [656.8, 720.0, 657.0, 714.0, 658.0, 709.0, 659.0, 704.0, 659.0, 699.0, 660.0, 694.0, 660.0, 689.0, 661.0, 684.0, 661.0, 679.0, 662.0, 674.0, 662.0, 669.0, 662.0, 664.0, 664.0, 659.0, 664.0, 654.0, 665.0, 649.0, 665.0, 644.0, 666.0, 639.0, 667.0, 634.0, 667.0, 629.0, 668.0, 624.0, 669.0, 619.0, 670.0, 614.0, 672.0, 609.0, 673.0, 604.0, 674.0, 599.0, 675.0, 594.0, 676.0, 589.0, 678.0, 584.0, 679.0, 579.0, 682.0, 574.0, 684.0, 569.0, 686.0, 564.0, 688.0, 559.0, 691.0, 554.0, 693.0, 549.0, 697.0, 544.0, 701.0, 539.0, 705.0, 534.0, 710.0, 529.0, 715.0, 524.0, 721.0, 519.0, 728.0, 514.0, 737.0, 509.0, 747.0, 504.0, 759.0, 499.0, 774.0, 494.0],
+                    "z_order": 0
+                }
+            ],
+            "attr": {"frame": 0},
+            "point_cloud": {"path": ""}
+        },
+        {
+            "id": "images/temporalRails/sequnce_1074_frame_026821",
+            "annotations": [
+                {
+                    "id": 0,
+                    "type": "polyline",
+                    "attributes": {"occluded": false},
+                    "group": 0,
+                    "label_id": 1,
+                    "points": [778.9, 720.0, 777.0, 714.0, 775.0, 709.0, 773.0, 704.0, 771.0, 699.0, 769.0, 694.0, 767.0, 689.0, 765.0, 684.0, 764.0, 679.0, 762.0, 674.0, 760.0, 669.0, 758.0, 664.0, 756.0, 659.0, 754.0, 654.0, 753.0, 649.0, 751.0, 644.0, 749.0, 639.0, 748.0, 634.0, 746.0, 629.0, 745.0, 624.0, 744.0, 619.0, 742.0, 614.0, 740.0, 609.0, 739.0, 604.0, 738.0, 599.0, 737.0, 594.0, 736.0, 589.0, 736.0, 584.0, 735.0, 579.0, 735.0, 574.0, 734.0, 569.0, 734.0, 564.0, 735.0, 559.0, 735.0, 554.0, 736.0, 549.0, 737.0, 544.0, 738.0, 539.0, 740.0, 534.0, 743.0, 529.0, 747.0, 524.0, 751.0, 519.0, 758.0, 514.0, 764.0, 509.0, 773.0, 504.0, 783.0, 499.0, 797.0, 494.0],
+                    "z_order": 0
+                },
+                {
+                    "id": 0,
+                    "type": "polyline",
+                    "attributes": {"occluded": false},
+                    "group": 0,
+                    "label_id": 0,
+                    "points": [657.9, 129.0, 720.0, 714.0, 658.0, 709.0, 659.0, 704.0, 659.0, 699.0, 660.0, 694.0, 660.0, 689.0, 661.0, 684.0, 661.0, 679.0, 662.0, 674.0, 662.0, 669.0, 662.0, 664.0, 664.0, 659.0, 664.0, 654.0, 665.0, 649.0, 665.0, 644.0, 666.0, 639.0, 667.0, 634.0, 667.0, 629.0, 668.0, 624.0, 668.0, 619.0, 670.0, 614.0, 671.0, 609.0, 672.0, 604.0, 673.0, 599.0, 674.0, 594.0, 676.0, 589.0, 677.0, 584.0, 679.0, 579.0, 681.0, 574.0, 683.0, 569.0, 685.0, 564.0, 688.0, 559.0, 690.0, 554.0, 693.0, 549.0, 696.0, 544.0, 701.0, 539.0, 704.0, 534.0, 710.0, 529.0, 715.0, 524.0, 721.0, 519.0, 729.0, 514.0, 737.0, 509.0, 748.0, 504.0, 759.0, 499.0, 775.0, 494.0],
+                    "z_order": 0
+                }
+            ],
+            "attr": {"frame": 1},
+            "point_cloud": {"path": ""}
+        }
+    ]
+}
+"""
+
+import json
+
+def analyze_polylines(file_path):
+    with open(file_path, 'r') as file:
+        json_data = json.load(file)
+    
+    images_with_polylines_not_starting_y720 = []
+    count_polylines_starting_y720 = 0
+    
+    for item in json_data["items"]:
+        for annotation in item["annotations"]:
+            points = annotation["points"]
+            if len(points) >= 2:
+                if points[1] == 720.0:  # Überprüfen, ob der zweite Wert im Punkte-Array 720.0 ist
+                    count_polylines_starting_y720 += 1
+                else:
+                    images_with_polylines_not_starting_y720.append(item["id"])
+                    break  # Stop processing further annotations for this item
+
+    return images_with_polylines_not_starting_y720, count_polylines_starting_y720
+
+# Beispiel: Pfad zur JSON-Datei angeben
+file_path = 'temporalSwitchDataset/annotations/default.json'
+images, count_y720 = analyze_polylines(file_path)
+
+# Ergebnisse ausgeben
+if images:
+    print("Bilder mit Polylinien, die nicht mit einem Punkt bei y=720.0 beginnen:")
+    for image in images:
+        print(image)
+else:
+    print("ALLE BILDER KORREKT GELABELT !!! :)")
+
+print(f"Anzahl der Polylinien, die mit einem Punkt bei y=720.0 beginnen: {count_y720}")
